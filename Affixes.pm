0224cf75f932c5bc48141a32cbd54c234491beba
José Alves de Castro <cog@cpan.org>
2004-10-23 17:18:41 +0000

    Import of COG/Text-Affixes-0.01 from CPAN.
    gitpan-cpan-distribution: Text-Affixes
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         COG/Text-Affixes-0.01.tar.gz
    gitpan-cpan-author:       COG
    gitpan-cpan-maturity:     released

---
+++ Affixes.pm
@@ -0,0 +1,122 @@
+package Text::Affixes;
+
+use 5.006;
+use strict;
+use warnings;
+
+require Exporter;
+use AutoLoader qw(AUTOLOAD);
+
+our @ISA = qw(Exporter);
+
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	get_prefixes
+	get_sufixes
+);
+
+our $VERSION = '0.01';
+
+sub get_prefixes {
+	my %conf = (min => 3, max => 3);
+	if (ref $_[0] eq 'HASH') {
+		%conf = (%conf, %{+shift});
+	}
+	my %elements;
+	my $text = shift || return undef;
+	$conf{min} = 1 if $conf{min} < 1;
+	for ($conf{min} .. $conf{max}) {
+	while ($text =~ /\b(\w{$_})\w/g) {
+			$elements{$_}{$1}++;
+		}
+	}
+	return \%elements;
+}
+
+sub get_sufixes {
+	my %conf = (min => 3, max => 3);
+	if (ref $_[0] eq 'HASH') {
+		%conf = (%conf, %{+shift});
+	}
+	my %elements;
+	my $text = shift || return undef;
+	$conf{min} = 1 if $conf{min} < 1;
+	for ($conf{min} .. $conf{max}) {
+	while ($text =~ /\w(\w{$_})\b/g) {
+			$elements{$_}{$1}++;
+		}
+	}
+	return \%elements;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Text::Affixes - Basis for Prefixes and Sufixes analisys of text
+
+=head1 SYNOPSIS
+
+  use Text::Affixes;
+  my $text = "Hello, world. Hello, big world.";
+  my $prefixes = get_prefixes($text);
+
+  # $prefixes now holds
+  # {
+  # 	3 => {
+  # 		'Hel' => 2,
+  # 		'wor' => 2,
+  # 	}
+  # }
+
+  # or
+
+  $prefixes = get_prefixes({min => 1, max => 2},$text);
+
+  # $prefixes now holds
+  # {
+  # 	1 => {
+  # 		'H' => 2,
+  # 		'w' => 2,
+  # 		'b' => 1,
+  # 	},
+  # 	2 => {
+  # 		'He' => 2,
+  # 		'wo' => 2,
+  # 		'bi' => 1,
+  # 	}
+  # }
+
+  # the use for get_sufixes is similar
+
+=head1 DESCRIPTION
+
+This module, still in its early stages, is meant to be used
+in Natural Language Processing tools that require Prefixe or
+Sufixe examination of text.
+
+=head1 TO DO
+
+* Improve the documentation
+
+* Make it more efficient (C?)
+
+=head1 AUTHOR
+
+Jose Alves de Castro, E<lt>cog@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2004 by Jose Castro
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.3 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
